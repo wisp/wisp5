d10891238683be946fd98367b271bc38eb4bd75a
Aaron Parks <anparks@uw.edu>
2014-08-29 12:58:31 -0700

    Updated accel and SPI modules in wisp-base to work with ADXL362Z

--- CCS/wisp-base/Sensors/accel.h
+++ CCS/wisp-base/Sensors/accel.h
@@ -1,9 +1,8 @@
 /**
- * @file accel.h
+ * accel.h
  *
- * Interface to the ADXL362 accelerometer driver
- *
- * @author Aaron Parks
+ *  @date Aug 2013
+ *  @author Aaron Parks
  */
 
 #ifndef ACCEL_H_
@@ -17,8 +16,18 @@ typedef struct {
     uint16_t z;
 } threeAxis_t;
 
+typedef struct {
+    uint8_t x;
+    uint8_t y;
+    uint8_t z;
+} threeAxis_t_8;
+
 BOOL ACCEL_initialize();
-BOOL ACCEL_singleSample(threeAxis_t* result);
+BOOL ACCEL_singleSample(threeAxis_t_8* result);
+BOOL ACCEL_readStat(threeAxis_t_8* result);
+BOOL ACCEL_readID(threeAxis_t_8* result);
+BOOL ACCEL_reset();
+BOOL ACCEL_range();
 
 
 #endif /* ACCEL_H_ */
