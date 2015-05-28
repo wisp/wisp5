a5722e049cf6ec14bf4126d99ba0719907803ce6
Ivar in 't Veen <ivarveen@gmail.com>
2015-05-28 11:48:59 +0200

    Add functions for setting ADC settings

--- CCS/wisp-base/Sensors/adc.c
+++ CCS/wisp-base/Sensors/adc.c
@@ -13,20 +13,31 @@
 struct {
     uint16_t lastValue; // Last read value (RAW bits)
 
-    void (*callback)(uint16_t); // Callback function for asynchronous measurements
+    ADC_referenceSelect reference;
+    ADC_precisionSelect precision;
+    ADC_inputSelect channel;
+
+    void (*read_callback)(uint16_t); // Callback function for asynchronous measurements
 } ADC_SM;
 
 /**
  * Configure the ADC12_B module in single channel single measurement mode and prepare for measurement.
- *
- * @todo Currently assumes all kinds of stuf. Make more flexible by accepting parameters/arguments.
  */
 void ADC_init(void) {
-    // Select reference voltage.
-    REFCTL0 = REFVSEL_1 + REFON + REFTCOFF;
+    ADC_initCustom(ADC_reference_2_0V, ADC_precision_10bit, ADC_input_A9);
+}
 
-    // Wait for REF to stabilize.
-    __delay_cycles(75 * 16);
+/**
+ * Configure the ADC12_B module in single channel single measurement mode and prepare for measurement.
+ *
+ * @param reference voltage reference source
+ * @param precision ADC measurement precision
+ * @param channel ADC input channel
+ */
+void ADC_initCustom(ADC_referenceSelect reference,
+        ADC_precisionSelect precision, ADC_inputSelect channel) {
+    // Select reference voltage.
+    ADC_setReference(reference);
 
     // Set registers to their reset conditions..
     ADC12CTL0 = 0;
@@ -36,42 +47,22 @@ void ADC_init(void) {
     ADC12IER0 = 0;
 
     // Turn on ADC.
-    ADC12CTL0 = ADC12ON;
+    ADC_enable();
 
     // Enable using sample and hold pulse mode and use clock as source.
-    ADC12CTL1 |= ADC12SHP + ADC12SSEL1;
-
-    // Enable TA0 CCR1 trigger
-    //ADC12CTL1 |= ADC12SHS_5;
+    ADC_setSampleHold();
 
     // Set resolution to 10 bits. (see user guide 25.3.3)
-    ADC12CTL2 |= ADC12RES__10BIT;
-
-    // Select VR+ = VREF and VR- = AVSS. (see user guide 25.3.6)
-    ADC12MCTL0 |= ADC12VRSEL_1;
+    ADC_setPrecision(precision);
 
     // enable interrupt
-    ADC12IER0 |= ADC12IE0;
-
-    __delay_cycles(75 * 16);
+    ADC_enableInterrupts();
 
     // Select analog input channel.
-    ADC12MCTL0 |= ADC12INCH_9;
-
-    // Select conversion sequence to single channel-single conversion. (see user guide 25.2.8)
-    ADC12CTL1 |= ADC12CONSEQ_0;
-
-    BITSET(PMEAS_ENOUT, PIN_MEAS_EN);
+    ADC_setInputChannel(channel);
 
     // Enable ADC conversions
-    ADC12CTL0 |= ADC12ENC;
-}
-
-/**
- * Return true if ADC module is in the middle of an conversion, false if not.
- */
-uint8_t ADC_busy(void) {
-    return (ADC12CTL1 & ADC12BUSY);
+    ADC_enableConversion();
 }
 
 /**
@@ -80,7 +71,7 @@ uint8_t ADC_busy(void) {
 uint16_t ADC_read(void) {
     ADC_asyncRead(0);
 
-    while (ADC_busy())
+    while (ADC_isBusy())
         ;
 
     return ADC_SM.lastValue;
@@ -92,7 +83,7 @@ uint16_t ADC_read(void) {
  * @param callback function to be called after conversion finishes
  */
 void ADC_asyncRead(void (*callback)(uint16_t)) {
-    ADC_SM.callback = callback;
+    ADC_SM.read_callback = callback;
     ADC12CTL0 |= ADC12SC;
 }
 
@@ -106,11 +97,173 @@ __interrupt void INT_ADC12(void) {
     case ADC12IV_ADC12IFG0:
         ADC_SM.lastValue = ADC12MEM0;
 
-        if (ADC_SM.callback)
-            (*ADC_SM.callback)(ADC_SM.lastValue);
+        if (ADC_SM.read_callback)
+            (*ADC_SM.read_callback)(ADC_SM.lastValue);
 
         break;
     default:
         break;
     }
+}
+
+/**
+ * Return true if ADC module is in the middle of a conversion, false if not.
+ */
+uint8_t ADC_isBusy(void) {
+    return !!(ADC12CTL1 & ADC12BUSY);
+}
+
+/**
+ * Return false if ADC module is in the middle of a conversion, true if not.
+ */
+uint8_t ADC_isReady(void) {
+    return !ADC_isBusy();
+}
+
+/**
+ * Enable the ADC12_B peripheral.
+ */
+void ADC_enable(void) {
+    // Enable ADC
+    ADC12CTL0 |= ADC12ON;
+}
+
+/**
+ * Disable the ADC12_B peripheral.
+ */
+void ADC_disable(void) {
+    ADC_disableConversion();
+
+    // Disable ADC conversions
+    ADC12CTL0 &= ~ADC12ENC;
+}
+
+/**
+ * Enable ADC conversions.
+ */
+void ADC_enableConversion(void) {
+    ADC_enable();
+
+    // Enable ADC conversions
+    ADC12CTL0 |= ADC12ENC;
+}
+
+/**
+ * Disable ADC conversions.
+ */
+void ADC_disableConversion(void) {
+    // Disable ADC
+    ADC12CTL0 &= ~ADC12ON;
+}
+
+/**
+ * Enable ADC interrupts.
+ */
+void ADC_enableInterrupts(void) {
+    // Enable ADC12IFG0 interrupt
+    ADC12IER0 |= ADC12IE0;
+}
+
+/**
+ * Disable all ADC interrupts.
+ */
+void ADC_disableInterrupts(void) {
+    // Disable all ADC interrupts
+    ADC12IER0 = 0;
+    ADC12IER1 = 0;
+    ADC12IER2 = 0;
+}
+
+/**
+ * Set voltage reference and make the ADC to use it.
+ *
+ * @param reference the wanted reference voltage.
+ */
+void ADC_setReference(ADC_referenceSelect reference) {
+    if (REFCTL0 & REFGENBUSY)
+        return;
+
+    ADC_SM.reference = reference;
+
+    // Set reference voltage.
+    REFCTL0 = reference + REFON + REFTCOFF;
+
+    // Wait for REF to stabilize.
+    __delay_cycles(75 * 16);
+
+    // Select VR+ = VREF and VR- = AVSS. (see user guide 25.3.6)
+    ADC12MCTL0 |= ADC12VRSEL_1;
+}
+
+/**
+ * Get currently set voltage reference.
+ */
+ADC_referenceSelect ADC_getReference(void) {
+    return ADC_SM.reference;
+}
+
+/**
+ * Set ADC conversion precision.
+ *
+ * @param precision the wanted precision.
+ */
+void ADC_setPrecision(ADC_precisionSelect precision) {
+    ADC_SM.precision = precision;
+
+    ADC12CTL2 |= precision;
+}
+
+/**
+ * Get currently set conversion precision.
+ */
+ADC_precisionSelect ADC_getPrecision(void) {
+    return ADC_SM.precision;
+}
+
+/**
+ * Set ADC input channel and single channel-single conversion mode.
+ *
+ * Possible channels: analog input pins A0-A31 and internal temperature sensor.
+ *
+ * @param channel the wanted channel.
+ */
+void ADC_setInputChannel(ADC_inputSelect channel) {
+    ADC_SM.channel = channel;
+
+    switch (channel) {
+    case ADC_input_temperature:
+        // Use alternate channel muxing
+        ADC12CTL3 |= ADC12TCMAP;
+
+        // Select temperature input channel
+        ADC12MCTL0 &= ~(0x1F);
+        ADC12MCTL0 |= ADC12INCH_30;
+        break;
+    default:
+        // Use default channel muxing
+        ADC12CTL3 &= ~ADC12TCMAP;
+
+        // Select analog input channel
+        ADC12MCTL0 &= ~(0x1F);
+        ADC12MCTL0 |= channel;
+        break;
+    }
+
+    // Select conversion sequence to single channel-single conversion. (see user guide 25.2.8)
+    ADC12CTL1 |= ADC12CONSEQ_0;
+}
+
+/**
+ * Get currently set input channel.
+ */
+ADC_inputSelect ADC_getInputChannel(void) {
+    return ADC_SM.channel;
+}
+
+/**
+ * Set ADC to sample and hold pulse mode.
+ */
+void ADC_setSampleHold(void) {
+    // Enable using sample and hold pulse mode and use clock as source.
+    ADC12CTL1 |= ADC12SHP + ADC12SSEL1;
 }
