a04ca76936df7cad1d8c5ce57a8138d35c4ec0e9
Saman Naderiparizi <samannp@uw.edu>
2015-11-09 18:55:38 -0800

    some codes uncommented that were commented for just the debug purposes

--- CCS/accelDemo/main.c
+++ CCS/accelDemo/main.c
@@ -59,19 +59,19 @@ void my_blockWriteCallback  (void) {
 void main(void) {
 
 	WISP_init();
-
-	// Accelerometer power up sequence
-//	BITCLR(POUT_ACCEL_EN , PIN_ACCEL_EN);
-//    CSCTL0_H = 0xA5;
-//    CSCTL1 = DCOFSEL_0; //1MHz
-//    CSCTL2 = SELA__VLOCLK + SELS_3 + SELM_3;
-//    CSCTL3 = DIVA_0 + DIVS_0 + DIVM_0;
-//	BITCLR(CSCTL6 , (MODCLKREQEN|SMCLKREQEN|MCLKREQEN));
-//	BITSET(CSCTL6 , ACLKREQEN);
-//
-//
-//	__delay_cycles(100);
 	BITSET(PDIR_ACCEL_EN , PIN_ACCEL_EN);
+	// Accelerometer power up sequence
+	BITCLR(POUT_ACCEL_EN , PIN_ACCEL_EN);
+    CSCTL0_H = 0xA5;
+    CSCTL1 = DCOFSEL_0; //1MHz
+    CSCTL2 = SELA__VLOCLK + SELS_3 + SELM_3;
+    CSCTL3 = DIVA_0 + DIVS_0 + DIVM_0;
+	BITCLR(CSCTL6 , (MODCLKREQEN|SMCLKREQEN|MCLKREQEN));
+	BITSET(CSCTL6 , ACLKREQEN);
+
+
+	__delay_cycles(100);
+
 	BITSET(POUT_ACCEL_EN , PIN_ACCEL_EN);
 
     // Register callback functions with WISP base routines
@@ -93,19 +93,19 @@ void main(void) {
 	accelOut.x = 1;
 	accelOut.y = 1;
 	accelOut.z = 1;
-//	BITSET(P2SEL1 , PIN_ACCEL_SCLK | PIN_ACCEL_MISO | PIN_ACCEL_MOSI);
-//	BITCLR(P2SEL0 , PIN_ACCEL_SCLK | PIN_ACCEL_MISO | PIN_ACCEL_MOSI);
-//	__delay_cycles(5);
-//	SPI_initialize();
-//	__delay_cycles(5);
-////	ACCEL_reset();
-////	__delay_cycles(50);
-//	ACCEL_range();
-//	__delay_cycles(5);
-//	ACCEL_initialize();
-//	__delay_cycles(5);
-//
-//	__delay_cycles(5);
+	BITSET(P2SEL1 , PIN_ACCEL_SCLK | PIN_ACCEL_MISO | PIN_ACCEL_MOSI);
+	BITCLR(P2SEL0 , PIN_ACCEL_SCLK | PIN_ACCEL_MISO | PIN_ACCEL_MOSI);
+	__delay_cycles(5);
+	SPI_initialize();
+	__delay_cycles(5);
+//	ACCEL_reset();
+//	__delay_cycles(50);
+	ACCEL_range();
+	__delay_cycles(5);
+	ACCEL_initialize();
+	__delay_cycles(5);
+
+	__delay_cycles(5);
 
     // Set up EPC, copy in sensor data
 	wispData.epcBuf[0] = 0x0B; // Tag type: Accelerometer
@@ -121,25 +121,25 @@ void main(void) {
 	wispData.epcBuf[10] = *((uint8_t*)INFO_WISP_TAGID+1); // WISP ID MSB: Pull from INFO seg
 	wispData.epcBuf[11] = *((uint8_t*)INFO_WISP_TAGID); // WISP ID LSB: Pull from INFO seg
 
+	ACCEL_singleSample(&accelOut);
+
+    BITSET(TA2CCTL0 , CCIE);
+    TA2CCR0 = 1000;
+    BITSET(TA2CTL , (TASSEL_1 | MC_1 | TACLR));
+    __bis_SR_register(LPM4_bits | GIE);       // Enter LPM4 w/ interrupt
+	__delay_cycles(1);
+    BITCLR(TA2CCTL0 , CCIE);
+    BITCLR(TA2CCR0 , 1000);
+    BITCLR(TA2CTL , (TASSEL_1 | MC_1 | TACLR));
+	ACCEL_readStat(&accelOut);
+	while((((uint8_t)accelOut.x) & 193) != 0x41){
+		ACCEL_readStat(&accelOut);
+		__delay_cycles(10);
+	}
+
 //	ACCEL_singleSample(&accelOut);
-//
-//    BITSET(TA2CCTL0 , CCIE);
-//    TA2CCR0 = 1000;
-//    BITSET(TA2CTL , (TASSEL_1 | MC_1 | TACLR));
-//    __bis_SR_register(LPM4_bits | GIE);       // Enter LPM4 w/ interrupt
-//	__delay_cycles(1);
-//    BITCLR(TA2CCTL0 , CCIE);
-//    BITCLR(TA2CCR0 , 1000);
-//    BITCLR(TA2CTL , (TASSEL_1 | MC_1 | TACLR));
-//	ACCEL_readStat(&accelOut);
-//	while((((uint8_t)accelOut.x) & 193) != 0x41){
-//		ACCEL_readStat(&accelOut);
-//		__delay_cycles(10);
-//	}
-//
-////	ACCEL_singleSample(&accelOut);
-//	__delay_cycles(10);
-//	ACCEL_singleSample(&accelOut);
+	__delay_cycles(10);
+	ACCEL_singleSample(&accelOut);
 	wispData.epcBuf[1] = 0;			// Y value MSB
 	wispData.epcBuf[2] = (accelOut.y+128);// Y value LSB
 	wispData.epcBuf[3] = 0;			// X value MSB
@@ -151,32 +151,32 @@ void main(void) {
 
     	WISP_doRFID();
 
-//        CSCTL0_H = 0xA5;
-//        CSCTL1 = DCOFSEL_0; //1MHz
-//        CSCTL2 = SELA__VLOCLK + SELS_3 + SELM_3;
-//        CSCTL3 = DIVA_0 + DIVS_0 + DIVM_0;
-//    	BITCLR(CSCTL6 , (MODCLKREQEN|SMCLKREQEN|MCLKREQEN));
-//    	BITSET(CSCTL6 , ACLKREQEN);
-//
-//		ACCEL_readStat(&accelOut);
-//		if((((uint8_t)accelOut.x) & 193) == 0x41){
-//			__delay_cycles(20);
-//			ACCEL_singleSample(&accelOut);
-//			wispData.epcBuf[1] = 0;			// Y value MSB
-//			wispData.epcBuf[2] = (accelOut.y+128);// Y value LSB
-//			wispData.epcBuf[3] = 0;			// X value MSB
-//			wispData.epcBuf[4] = (accelOut.x+128);// X value LSB
-//			wispData.epcBuf[5] = 0;			// Z value MSB
-//			wispData.epcBuf[6] = (accelOut.z+128);// Z value LSB
-//		}
+        CSCTL0_H = 0xA5;
+        CSCTL1 = DCOFSEL_0; //1MHz
+        CSCTL2 = SELA__VLOCLK + SELS_3 + SELM_3;
+        CSCTL3 = DIVA_0 + DIVS_0 + DIVM_0;
+    	BITCLR(CSCTL6 , (MODCLKREQEN|SMCLKREQEN|MCLKREQEN));
+    	BITSET(CSCTL6 , ACLKREQEN);
 
-//		BITSET(TA2CCTL0 , CCIE);
-//	    TA2CCR0 = 20;
-//	    BITSET(TA2CTL , (TASSEL_1 | MC_1 | TACLR));
-//	    __bis_SR_register(LPM4_bits | GIE);       // Enter LPM4 w/ interrupt
-//		__delay_cycles(1);
-//	    BITCLR(TA2CCTL0 , CCIE);
-//	    BITCLR(TA2CTL , (TASSEL_1 | MC_1 | TACLR));
+		ACCEL_readStat(&accelOut);
+		if((((uint8_t)accelOut.x) & 193) == 0x41){
+			__delay_cycles(20);
+			ACCEL_singleSample(&accelOut);
+			wispData.epcBuf[1] = 0;			// Y value MSB
+			wispData.epcBuf[2] = (accelOut.y+128);// Y value LSB
+			wispData.epcBuf[3] = 0;			// X value MSB
+			wispData.epcBuf[4] = (accelOut.x+128);// X value LSB
+			wispData.epcBuf[5] = 0;			// Z value MSB
+			wispData.epcBuf[6] = (accelOut.z+128);// Z value LSB
+		}
+
+		BITSET(TA2CCTL0 , CCIE);
+	    TA2CCR0 = 20;
+	    BITSET(TA2CTL , (TASSEL_1 | MC_1 | TACLR));
+	    __bis_SR_register(LPM4_bits | GIE);       // Enter LPM4 w/ interrupt
+		__delay_cycles(1);
+	    BITCLR(TA2CCTL0 , CCIE);
+	    BITCLR(TA2CTL , (TASSEL_1 | MC_1 | TACLR));
    }
 }
 
